7dd02f2aa32cc6a35c49e061a4a7f9ff258741f9
BigZuZu <abduraimov.1992@gmail.com>
2021-02-26 07:38:13 +0300

    сообщение

---
+++ projects/test-lib/src/lib/list/list/list.component.ts
@@ -0,0 +1,22 @@
+import { ChangeDetectionStrategy, Component, Input, OnInit } from "@angular/core";
+
+@Component({
+  selector: 'tl-list',
+  template: `
+    <ul>
+      <li *ngFor="let item of list">{{item}}</li>
+    </ul>`,
+  styleUrls: ['./list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ListComponent implements OnInit {
+
+  @Input()
+  public list: string[] = [];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListModule } from '@aziz-abduraimov/test-lib';
+import { BorderModule } from '@aziz-abduraimov/test-lib';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ListModule,
+    BorderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
